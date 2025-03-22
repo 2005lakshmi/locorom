0cf9f0bdb4d03f1c876334e81f5216cb93765f64
2005lakshmi <145553662+2005lakshmi@users.noreply.github.com>
2025-03-22 06:47:54 +0530

    Update check.py

--- check.py
+++ check.py
@@ -910,7 +910,7 @@ def default_page():
     selected_room = st.radio("Select Room", filtered_rooms)
     st.markdown("<hr style='border: 1px solid gray; margin: 0px 0;'>", unsafe_allow_html=True)
 
-    st.subheader(f"## Room: :red[{selected_room}]")
+    st.header(f"Room: :red[{selected_room}]")
 
     # Display main content
     display_main_content(selected_room)
