ca9cd3d3e38a9813b97719337d3827372fcb8e7d
2005lakshmi <145553662+2005lakshmi@users.noreply.github.com>
2025-03-22 06:26:31 +0530

    Update check.py

--- check.py
+++ check.py
@@ -244,7 +244,8 @@ def display_main_content(room_name):
             first_file = main_media[0]
             st.image(first_file['download_url'], width=200)
         with col2:
-            st.markdown(f"##### Location Info :")
+            st.markdown("<h5 style='color: royalblue;'>Location Info :</h5>", unsafe_allow_html=True)
+
             st.markdown(f"###### {info_content}")
             
         # Main Area Carousel
@@ -270,38 +271,8 @@ def display_main_content(room_name):
         col1, col2 = st.columns([2, 3])
         with col1:
             thumbnail_url = f"https://raw.githubusercontent.com/{GITHUB_REPO}/main/{sub_path}/thumbnail.jpg"
-            #st.image(thumbnail_url, width=200)
-            st.markdown(
-    f"""
-    <style>
-    .image-container {{
-        display: flex;
-        justify-content: center;
-    }}
-
-    .glowing-border {{
-        width: 200px; /* Same as Streamlit image width */
-        height: auto;
-        border-radius: 10px;
-        padding: 5px;
-        background: linear-gradient(45deg, #3D3D3D,5D8736,#3D3D3D);
-        background-size: 200% 200%;
-        animation: glowing 3s infinite linear;
-    }}
-
-    @keyframes glowing {{
-        0% {{ background-position: 0% 50%; }}
-        50% {{ background-position: 100% 50%; }}
-        100% {{ background-position: 0% 50%; }}
-    }}
-    </style>
-
-    <div class="image-container">
-        <img src="{thumbnail_url}" class="glowing-border">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+            st.image(thumbnail_url, width=200)
+            
 
         with col2:
             sub_info = get_subfolder_info(room_name, sub)
